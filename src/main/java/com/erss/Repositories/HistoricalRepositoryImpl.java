3530616cfca782f423b4719ccf660aad108c6559
daraiing <darathorn_titi@hotmail.com>
2015-11-30 16:49:40 +0700

    update after reset

--- src/main/java/com/erss/Repositories/HistoricalRepositoryImpl.java
+++ src/main/java/com/erss/Repositories/HistoricalRepositoryImpl.java
@@ -3,27 +3,41 @@ package com.erss.Repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 
 import com.erss.Models.Historical;
+import com.erss.Models.Student;
 
 public class HistoricalRepositoryImpl implements HistoricalRepository {
 
-
 	MongoTemplate mongoTemplate;
-	
+
 	public void setMongoTemplate(MongoTemplate mongoTemplate) {
 		this.mongoTemplate = mongoTemplate;
 	}
 
 	@Override
 	public boolean insert(Historical h) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			mongoTemplate.save(h);
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 	}
+
 	@Override
 	public boolean delete(Historical h) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			BasicQuery query = new BasicQuery("{ $and"
+					+ "[{sid:'" + h.getSid() + "'},"
+					+ "{cid:'" + h.getCid()+ "'}]");
+			mongoTemplate.remove(query, Historical.class);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
