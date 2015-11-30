af138641b85bd6d74f76a8c0990ca7041848627e
tapood <tapood_pony@hotmail.com>
2015-11-30 13:57:19 +0700

    #updated

---
+++ src/main/java/com/erss/Models/Message.java
@@ -0,0 +1,20 @@
+package com.erss.Models;
+
+public class Message {
+	private String msg_type;
+	private Object msg_content;
+	public String getMsg_type() {
+		return msg_type;
+	}
+	public void setMsg_type(String msg_type) {
+		this.msg_type = msg_type;
+	}
+	public Object getMsg_content() {
+		return msg_content;
+	}
+	public void setMsg_content(Object msg_content) {
+		this.msg_content = msg_content;
+	}
+	
+	
+}
