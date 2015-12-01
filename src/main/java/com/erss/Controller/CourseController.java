01ea30ce402066e18cc84f196a9ae5bbae95dd01
daraiing <darathorn_titi@hotmail.com>
2015-12-01 16:42:04 +0700

    #update servlet.xml

--- src/main/java/com/erss/Controller/CourseController.java
+++ src/main/java/com/erss/Controller/CourseController.java
@@ -3,6 +3,7 @@ package com.erss.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,17 +26,17 @@ public class CourseController {
 	return msg;
 	}
 	
-	@RequestMapping(value="delete",method = RequestMethod.POST)
-	public Object deleteCourse(@ModelAttribute Course c)
+	@RequestMapping(value="delete",method = RequestMethod.GET)
+	public @ResponseBody Message deleteCourse(@ModelAttribute Course c)
 	{
 		Message msg = courseService.delete(c);
 		return msg;
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
-	public Object updateCourse(@ModelAttribute Course c)
+	@RequestMapping(method = RequestMethod.PATCH)
+	public Object updateCourse(@PathVariable("cid") String cid)
 	{
-		Message msg = courseService.update(c);
+		Message msg = courseService.update((Course) courseService.findOne(cid).getMsgContent());
 		return msg;
 //		return null;
 	}
--- src/main/java/com/erss/Controller/HomeController.java
+++ src/main/java/com/erss/Controller/HomeController.java
@@ -1,26 +1,15 @@
 package com.erss.Controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erss.Models.Course;
 import com.erss.Models.Message;
-import com.erss.Models.Student;
 import com.erss.Services.CourseService;
 import com.erss.Services.StudentService;
-import com.erss.Util.Crypto;
 
 /**
  * Handles requests for the application home page.
