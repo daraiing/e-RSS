ed2fa0cc0eda9e098bfcc546f9b91b41287cc62c
narongritp <narongrit.pa@outlook.com>
2015-12-01 17:13:06 +0700

    #add MessageGenericException to com.erss.Exception

---
+++ src/main/java/com/erss/Exception/MessageGenericException.java
@@ -0,0 +1,30 @@
+package com.erss.Exception;
+
+public class MessageGenericException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final String ERR_404 = "404";
+	public static final String ERR_500 = "500";
+	
+	private String errid;
+	private String errmsg;
+	public MessageGenericException(String errid,String errmsg) {
+		this.errid = errid;
+		this.errmsg = errmsg;
+	}
+	public String getErrid() {
+		return errid;
+	}
+	public void setErrid(String errid) {
+		this.errid = errid;
+	}
+	public String getErrmsg() {
+		return errmsg;
+	}
+	public void setErrmsg(String errmsg) {
+		this.errmsg = errmsg;
+	}
+	
+	
+}
