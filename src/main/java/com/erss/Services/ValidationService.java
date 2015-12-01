2056c9d0639d83c703b0fff21172667b9f9bf3d3
tapood <tapood_pony@hotmail.com>
2015-12-01 09:25:47 +0700

    #tick ValidationService

---
+++ src/main/java/com/erss/Services/ValidationService.java
@@ -0,0 +1,7 @@
+package com.erss.Services;
+
+import com.erss.Models.Message;
+
+public interface ValidationService {
+	public Message validation(String sid,String password);
+}
---
+++ src/main/java/com/erss/Services/ValidationServiceImpl.java
@@ -0,0 +1,35 @@
+package com.erss.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.erss.Models.Message;
+import com.erss.Models.Student;
+import com.erss.Repositories.StudentRepository;
+import com.erss.Util.Crypto;
+
+public class ValidationServiceImpl implements ValidationService {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Override
+	public Message validation(String sid, String password) {
+		Student st = studentRepository.findOne(sid);
+		if (st != null) {
+			if (st.getPassword().equals(Crypto.SHA1(password))) {
+				Message msg = new Message(Message.MSG_SUCCESS);
+				msg.setMsgContent("Welcome\n" + st);
+				return msg;
+			} else {
+				Message msg = new Message(Message.MSG_ERROR);
+				msg.setMsgContent("Password is wrong");
+				return msg;
+			}
+		} else {
+			Message msg = new Message(Message.MSG_ERROR);
+			msg.setMsgContent("Student ID is invalid.");
+			return msg;
+		}
+	}
+
+}
