3ce3815479754e4a401a9edb948aca14f5a31dee
tapood <tapood_pony@hotmail.com>
2015-12-01 09:06:51 +0700

    #create validationService #fix xml for validationService

--- src/main/java/com/erss/Controller/StudentController.java
+++ src/main/java/com/erss/Controller/StudentController.java
@@ -9,16 +9,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.erss.Models.Message;
 import com.erss.Models.Student;
 import com.erss.Services.StudentService;
+import com.erss.Services.ValidationService;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 	@Autowired
 	StudentService studentService;
+	@Autowired
+	ValidationService validationService;
+	
 	@RequestMapping(value="insert", method= RequestMethod.POST)
 	public @ResponseBody Message insertStudent(Student student)
 	{
 		Message message = studentService.insert(student);
+		return message;
+	}
+	
+	@RequestMapping(value="check", method= RequestMethod.POST)
+	public @ResponseBody Message validation(Student student)
+	{
+		Message message = validationService.validation(student.getSid(), student.getPassword());
 		return message;
 	}
 
