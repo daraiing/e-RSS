ba98b20f0b800a2d20512340c4e09c806ba28788
tapood <tapood_pony@hotmail.com>
2015-12-01 09:23:43 +0700

    #update validationService

--- src/main/java/com/erss/Controller/StudentController.java
+++ src/main/java/com/erss/Controller/StudentController.java
@@ -2,6 +2,7 @@ package com.erss.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,14 +21,14 @@ public class StudentController {
 	ValidationService validationService;
 	
 	@RequestMapping(value="insert", method= RequestMethod.POST)
-	public @ResponseBody Message insertStudent(Student student)
+	public @ResponseBody Message insertStudent(@ModelAttribute Student student)
 	{
 		Message message = studentService.insert(student);
 		return message;
 	}
 	
 	@RequestMapping(value="check", method= RequestMethod.POST)
-	public @ResponseBody Message validation(Student student)
+	public @ResponseBody Object validation(@ModelAttribute Student student)
 	{
 		Message message = validationService.validation(student.getSid(), student.getPassword());
 		return message;
