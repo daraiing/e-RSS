632bf5ba8777e60b9d3832a66a963fc859bcc523
narongritp <narongrit.pa@outlook.com>
2015-12-01 14:38:01 +0700

    #insert code to function{Teacher,TeacherRepo,TeacherService}

--- src/main/java/com/erss/Services/TeacherService.java
+++ src/main/java/com/erss/Services/TeacherService.java
@@ -1,13 +1,10 @@
 package com.erss.Services;
 
-import java.util.List;
-
 import com.erss.Models.Message;
 import com.erss.Models.Teacher;
 
 public interface TeacherService {
 	
-	public Message Message(String tid);
 	public Message find(Teacher t);
 	public Message insert(Teacher t);
 	public Message delete(Teacher t);
