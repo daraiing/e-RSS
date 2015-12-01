d50cc23dff13ca4fae9d73dfee0b85d9e7058281
narongritp <narongrit.pa@outlook.com>
2015-12-01 09:32:55 +0700

    #insert code teacher repoฯ

--- src/main/java/com/erss/Repositories/TeacherRepositoryImpl.java
+++ src/main/java/com/erss/Repositories/TeacherRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.erss.Repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
+import org.springframework.data.mongodb.core.query.Update;
 
 import com.erss.Models.Teacher;
 
@@ -16,31 +18,58 @@ public class TeacherRepositoryImpl implements TeacherRepository {
 
 	@Override
 	public Teacher fineOne(String tid) {
-		// TODO Auto-generated method stub
-		return null;
+		BasicQuery query = new BasicQuery("{ tid:'" + tid + "'}");
+		return mongoTemplate.findOne(query, Teacher.class);
 	}
 
 	@Override
 	public boolean insert(Teacher t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			mongoTemplate.save(t);
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 	}
 	@Override
 	public boolean delete(Teacher t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			BasicQuery query = new BasicQuery("{ tid:'" + t.getTid() + "'}");
+			mongoTemplate.remove(query, Teacher.class);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean update(Teacher t) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			BasicQuery query = new BasicQuery("{ tid:'" + t.getTid() + "'}");
+			Update update = new Update();
+			update.set("fname",t.getFname());
+			update.set("lname",t.getLname());
+			update.set("field",t.getField());
+			update.set("password",t.getPassword());
+			update.set("title", t.getTitle());
+			mongoTemplate.updateFirst(query, update, Teacher.class);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	@Override
 	public List<Teacher> find(Teacher t) {
-		// TODO Auto-generated method stub
-		return null;
+		BasicQuery query = new BasicQuery("{ $or: [ { tid	:'"+ t.getTid()  	+"' },"
+				+ "	{ fname	: '" + t.getFname() + "' },"
+				+ " { lname : '" + t.getLname()	+ "' },"
+				+ " { gender: '" + t.getGender()+ "'  },"
+				+ " { title : '" + t.getTitle()	+ "' },"
+				+ " { field : '" + t.getField()	+ "' }"
+				+ "]}");
+			return mongoTemplate.find(query,Teacher.class);
 	}
 
 }
